e56c529381571fbb254f020e03e68a1a89a01e09
K. H <knhg2013@gmail.com>
2019-01-27 15:45:59 -0500

    1)Create an emply MongoDb in mLab, and connect to this NodeJS API. 2)Complete the create route/function.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,9 +1,16 @@
 
 module.exports = function(app,db){
     app.post('/notes', (req, res) => {
-       //  It creates the note here
-        console.log(req.body);
-       res.send('Hello');
+        // const note works like a payload in the request body
+        const note = { text: req.body.body , title : req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({'error': 'An error has occurred'});
+            }
+            else {
+                res.send(result.ops[0]);
+            }
+        })
     });
 
 }
--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const express       =   require('express');
 const MongoClient   =   require('mongodb').MongoClient;
 const bodyParser    =   require('body-parser');
 
-const app           =   express();
+// ./config/db.js is not committed. This file save the db connection url
+const db            =   require('./config/db');
 
+const app           =   express();
 
 const port  =   8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
-
-app.listen(port, () => {
-   console.log('We are live on ' + port) ;
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err);
+    dbb = database.db("notable");
+    require('./app/routes')(app, dbb);
+    app.listen(port, () => {
+        console.log('We are live on ' + port) ;
+    });
 });

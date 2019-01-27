75f6d52c83322d9526bdec2a4bd56dc2c636d804
K. H <knhg2013@gmail.com>
2019-01-27 16:58:53 -0500

    1)Complete the Read,Update,and Delete func/routes.

---
+++ README.md
@@ -0,0 +1,19 @@
+
+<h2> This project is planned to complete a MEAN stack project. </h2>
+<h4> --- Currently working on APIs. Partially done.</h4>
+The data come from a MongoDb, via mLab.
+
+The APIs are built via NodeJS and Express framework.
+
+In addition, the Single Page Application is built via Angular 6/7.
+
+
+
+
+To execute this project, it needs to install the following node modules : ExpressJS, MongoDb, body-parser, and so on.
+
+Also, it is suggested to install nodemon. So that, the API can restart you server when file changes.
+
+
+
+<h4>The project is under construction now.</h4>
--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,52 @@
 
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app,db){
+
+    // the function of "update"
+    app.put( '/notes/:id', (req, res) => {
+        const id        =   req.params.id;
+        const details   =   {'_id' : new ObjectID(id)};
+        const note      =   { text: req.body.body, title: req.body.title };
+        db.collection('notes').update(details, note, (err, result) => {
+            if (err){
+                res.send({'error' : 'An error has occurred'});
+            }
+            else {
+                res.send(note);
+            }
+        });
+    });
+
+    // the function of "delete"
+    app.delete('/notes/:id', (req, res) => {
+        const id        =   req.params.id;
+        const details   =   {'_id' : new ObjectID(id)};
+        db.collection('notes').remove(details, (err, item) => {
+            if (err){
+                res.send({'error' : 'An error has occurred'});
+            }
+            else {
+                res.send('Note ' + id + ' deleted!');
+            }
+        });
+    });
+
+    // the function of "read"
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id)}; // "details" is the request payload
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occurred'});
+            }
+            else {
+                res.send(item);
+            }
+        });
+    });
+
+    // the function of "create"
     app.post('/notes', (req, res) => {
         // const note works like a payload in the request body
         const note = { text: req.body.body , title : req.body.title };
